0405a01d49ba935d2525883a4a3872e5c8f941e7
Deshan Kodithuwakku <62970260+deshankodithuwakku@users.noreply.github.com>
2025-03-22 11:53:58 +0530

    update

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5001;
 
 export const mongoDBURL =
-  'mongodb+srv://nisal:nisal@cluster0.p860x.mongodb.net/travel?retryWrites=true&w=majority&appName=Cluster0';
+  'mongodb+srv://deshan:9hoWrvS5SPuoYH63@cluster0.kbifyf0.mongodb.net/travel?retryWrites=true&w=majority&appName=Cluster0';
 
 export const JWT_SECRET = '123456'; // replace with a secure secret in production
 
